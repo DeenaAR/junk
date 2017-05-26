43f8fe1e78f6ba27385bab5d96fd5314bd2245ec
DeenaAR <deena@deenasdomain.com>
2017-05-25 23:12:54 -0400

    other personal things

--- shell/git_config.sh
+++ shell/git_config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 # This just configs some nice global git stuffs
 
-git config --global user.name "Carson Gee"
-git config --global user.email "x@carsongee.com"
+git config --global user.name "Deena Rennerfeldt"
+git config --global user.email "deena@deenasdomain.com"
 git config --global color.ui auto
 git config --global alias.st status
 git config --global alias.co checkout
